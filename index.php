e44ff00aa36b525d49cf016b26716eccc8db143c
Yasuo Harada <slywalker.net@gmail.com>
2011-08-27 20:35:47 +0900

    add amazon

--- index.php
+++ index.php
@@ -15,5 +15,6 @@
 	</ul>
 	<iframe src="http://rcm-jp.amazon.co.jp/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=slywalker-22&o=9&p=8&l=as4&m=amazon&f=ifr&ref=ss_til&asins=4774144371" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
 	<iframe src="http://rcm-jp.amazon.co.jp/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=slywalker-22&o=9&p=8&l=as4&m=amazon&f=ifr&ref=ss_til&asins=4883374718" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
+	<iframe src="http://rcm-jp.amazon.co.jp/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=slywalker-22&o=9&p=8&l=as4&m=amazon&f=ifr&ref=ss_til&asins=4839930651" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
 </body>
 </html>
