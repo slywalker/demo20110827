3ac4a151a1f571d518eb22440f34469dd31d465a
Yasuo Harada <slywalker.net@gmail.com>
2011-08-27 14:57:34 +0900

    add php link

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 <body>
 	<h1>Demo 2011-08-27</h1>
 	<ul>
+		<li><a href="http://jp2.php.net/manual/ja/ref.image.php">PHP: GD および Image 関数 - Manual</a></li>
 		<li><a href="resized_vs_sampled.php">imagecopyresized vs imagecopyresampled</a></li>
 		<li><a href="imagefilter.php">imagefilter</a></li>
 		<li><a href="add_png.php">add png</a></li>
